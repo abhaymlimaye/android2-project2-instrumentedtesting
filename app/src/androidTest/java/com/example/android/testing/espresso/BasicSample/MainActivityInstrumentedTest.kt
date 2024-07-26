80ffe3fd2796f90a2bf87930a3add8b4dc3ed6b2
abhaymlimaye <32418776+abhaymlimaye@users.noreply.github.com>
2024-07-26 14:16:14 -0400

    Adds a test for "Hello World" text in TextView

--- app/src/androidTest/java/com/example/android/testing/espresso/BasicSample/MainActivityInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/android/testing/espresso/BasicSample/MainActivityInstrumentedTest.kt
@@ -1,4 +1,19 @@
 package com.example.android.testing.espresso.BasicSample
 
+import androidx.test.core.app.ActivityScenario
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
 class MainActivityInstrumentedTest {
+    @Test
+    fun validateTextView() {
+        ActivityScenario.launch(MainActivity::class.java)
+        onView(withId(R.id.textToBeChanged))
+            .check(matches(withText("Hello World!")))
+    }
 }
