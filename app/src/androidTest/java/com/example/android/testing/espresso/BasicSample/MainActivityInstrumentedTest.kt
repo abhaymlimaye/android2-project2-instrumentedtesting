8028240e6c7b46cb8732c9d49a07c349a2f7d802
abhaymlimaye <32418776+abhaymlimaye@users.noreply.github.com>
2024-07-26 14:18:41 -0400

    Adds a test for Entering “123” and pressing Change Text button

--- app/src/androidTest/java/com/example/android/testing/espresso/BasicSample/MainActivityInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/android/testing/espresso/BasicSample/MainActivityInstrumentedTest.kt
@@ -3,6 +3,8 @@ package com.example.android.testing.espresso.BasicSample
 import androidx.test.core.app.ActivityScenario
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.replaceText
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import org.junit.Test
@@ -10,10 +12,23 @@ import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class MainActivityInstrumentedTest {
+
     @Test
     fun validateTextView() {
         ActivityScenario.launch(MainActivity::class.java)
         onView(withId(R.id.textToBeChanged))
             .check(matches(withText("Hello World!")))
     }
+
+    @Test
+    fun enter123AndChangeText() {
+        ActivityScenario.launch(MainActivity::class.java)
+        onView(withId(R.id.editTextUserInput))
+            .perform(replaceText("123"))
+        onView(withId(R.id.changeTextBt))
+            .perform(click())
+        onView(withId(R.id.textToBeChanged))
+            .check(matches(withText("123")))
+    }
+
 }
